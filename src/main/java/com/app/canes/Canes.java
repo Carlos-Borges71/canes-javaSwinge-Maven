d0c4f6550fd7172b2df4cfd6e658d99c9ead7957
Carlos Borges <kaca_master@hotmail.com>
2025-11-18 22:22:29 -0300

    projeto criado com classe de usuario

---
+++ src/main/java/com/app/canes/Canes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.app.canes;
+
+/**
+ *
+ * @author Carlos Borges
+ */
+public class Canes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/app/canes/model/Usuario.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.app.canes.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Carlos Borges
+ */
+public class Usuario {
+    private int id;
+    private String setor;
+    private String nome;
+    private String login;
+    private Date data;
+    private String senha;
+    
+    public Usuario(){        
+    }
+
+    public Usuario(int id, String setor, String nome, String login, Date data, String senha) {
+        this.id = id;
+        this.setor = setor;
+        this.nome = nome;
+        this.login = login;
+        this.data = data;
+        this.senha = senha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Usuario other = (Usuario) obj;
+        return this.id == other.id;
+    }
+    
+    
+}
